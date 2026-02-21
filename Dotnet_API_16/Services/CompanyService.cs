2c4130a3ba094a730cbc1dafac9ab52a31c92102
tapan0810 <arceus0810@gmail.com>
2026-02-21 20:03:10 +0530

    feat: Implemented Service and Interface for this

---
+++ Dotnet_API_16/Services/CompanyService.cs
@@ -0,0 +1,107 @@
+﻿using Dotnet_API_16.Data;
+using Dotnet_API_16.Entities.Dtos;
+using Dotnet_API_16.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_16.Services
+{
+    public class CompanyService(CompanyDbContext _context) : ICompanyService
+    {
+        public async Task<GetCompanyByIdDtocs?> AddComapny(AddCompanyDto addCompanyDto)
+        {
+            var company = new Company
+            {
+                CompanyName = addCompanyDto.CompanyName,
+                CompanyAddress = addCompanyDto.CompanyAddress,
+                CompanyCity = addCompanyDto.CompanyCity,
+                CompanyOwner = addCompanyDto.CompanyOwner,
+                IsActive = addCompanyDto.IsActive
+            };
+
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return new GetCompanyByIdDtocs
+            {
+                CompanyId = company.CompanyId,
+                CompanyName = company.CompanyName,
+                CompanyAddress = company.CompanyAddress,
+                CompanyCity = company.CompanyCity,
+                CompanyOwner = company.CompanyOwner,
+                IsActive = company.IsActive
+            };
+
+        }
+
+        public async Task<bool> DeleteCompany(int id)
+        {
+            var comapny = await _context.Companies.FirstOrDefaultAsync(x =>x.CompanyId == id);
+            if (comapny is null)
+                return false;
+
+            _context.Companies.Remove(comapny);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public Task<List<GetAllCompaniesDto>> GetAllCompaniesAsync()
+        {
+            var companies = _context.Companies
+                .Select(s => new GetAllCompaniesDto
+                {
+                    CompanyName = s.CompanyName,
+                    CompanyAddress = s.CompanyAddress,
+                    CompanyCity = s.CompanyCity,
+                    CompanyOwner = s.CompanyOwner,
+                    IsActive = s.IsActive
+                }
+                ).ToListAsync();
+
+            if(companies == null)
+            {
+                return null;
+            }
+
+            return companies;
+
+        }
+
+        public async Task<GetCompanyByIdDtocs?> GetCompanyById(int id)
+        {
+            var company = await _context.Companies
+                 .Where(x => x.CompanyId == id)
+                 .Select(s => new GetCompanyByIdDtocs
+                 {
+                     CompanyId = s.CompanyId,
+                     CompanyName = s.CompanyName,
+                     CompanyAddress = s.CompanyAddress,
+                     CompanyCity = s.CompanyCity,
+                     CompanyOwner = s.CompanyOwner,
+                     IsActive = s.IsActive
+
+                 }).FirstOrDefaultAsync();
+
+            if (company is null)
+                return null;
+
+            return company;
+        }
+
+        public async Task<bool> UpdateCompany(int id, UpdateCompanyDto updateCompanyDto)
+        {
+            var company = await _context.Companies.FirstOrDefaultAsync(x => x.CompanyId == id);
+
+            if (company is null)
+                return false;
+
+            company.CompanyName = updateCompanyDto.CompanyName;
+            company.CompanyAddress = updateCompanyDto.CompanyAddress;
+            company.CompanyCity = updateCompanyDto.CompanyCity;
+            company.CompanyOwner = updateCompanyDto.CompanyOwner;
+            company.IsActive = updateCompanyDto.IsActive;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Dotnet_API_16/Services/ICompanyService.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet_API_16.Entities.Dtos;
+
+namespace Dotnet_API_16.Services
+{
+    public interface ICompanyService
+    {
+        public Task<List<GetAllCompaniesDto>> GetAllCompaniesAsync();
+        public Task<GetCompanyByIdDtocs?> GetCompanyById(int id);
+        public Task<GetCompanyByIdDtocs?> AddComapny(AddCompanyDto addCompanyDto);
+        public Task<bool> UpdateCompany(int id, UpdateCompanyDto updateCompanyDto);
+        public Task<bool>DeleteCompany(int id);
+    }
+}
